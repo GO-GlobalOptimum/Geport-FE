9e974faf0ec91c5f88ddc1d8ebe06a4e9342f626
youhyunwoo99 <casshyunwoo@naver.com>
2024-05-19 22:24:35 +0900

    [ADD] create_post, posts page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { Link, Route, Routes , BrowserRouter} from 'react-router-dom';
 import {Login} from "./component/login/Login";
 import {Landing} from "./component/landing/Landing";
 import {MyPage} from './component/mypage/MyPage';
-import {IGeport} from "./component/Igeport/IGeport";
+import {IGeport} from "./component/Igeport/detail/IGeport";
 import {Igeport_question} from "./component/Igeport_question/Igeport_question";
 import {Igeport_question1} from "./component/Igeport_question/detail/Igeport_question1";
-
+import {Create_post} from "./component/create_post/create_post";
+import {Posts}from"./component/posts/Posts";
+import { My_Posts } from "./component/posts/detail/My_Posts";
+import { Other_Posts } from "./component/posts/detail/Other_Posts";
 function App() {
   return (
     <BrowserRouter>
@@ -17,7 +20,10 @@ function App() {
             <Route path="/mypage" element={<MyPage />}></Route>
             <Route path="/igeport" element={<IGeport />}></Route>
             <Route path="/igeport/question" element={<Igeport_question/>}></Route>
-
+            <Route path="/create_post" element={<Create_post/>}></Route>
+            <Route path="/posts" element={<Posts/>}></Route>
+            <Route path="/my_posts" element={<My_Posts/>}></Route>
+            <Route path="/other_posts" element={<Other_Posts/>}></Route>
         </Routes>
     </BrowserRouter>
   );
--- src/component/Igeport/detail/IGeport.js
+++ src/component/Igeport/detail/IGeport.js
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from 'react';
 import {useNavigate} from "react-router-dom";
 
 
-export function Landing(props){
+export function IGeport(props){
     const [isModalOpen, setModalOpen] = useState(false);
 
     const openModal = () => setModalOpen(true);
     const closeModal = () => setModalOpen(false);
 
-    return (
-        <div>
-            <LandingHeader openModal={openModal}/>
-            <HotPage/>
-            <Modal isOpen={isModalOpen} onClose={closeModal}>
-                <Login/>
-            </Modal>
-        </div>
-    )
+    // return (
+    //     // <div>
+    //     //     <LandingHeader openModal={openModal}/>
+    //     //     <HotPage/>
+    //     //     <Modal isOpen={isModalOpen} onClose={closeModal}>
+    //     //         <Login/>
+    //     //     </Modal>
+    //     // </div>
+    // )
 }
