f5840b64a2cd9d15340efcb5cf7274a520e83ae7
whitekuj02 <whitekuj02@naver.com>
2024-05-26 16:35:08 +0900

    fix : build 오류 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         "/BE",
         createProxyMiddleware({
-            target: process.env.REACT_APP_BACKEND_ENDPOINT,
+            target: "https://geport.blog",
             changeOrigin: true,
             // pathRewrite: {
             //     '^/api': '' // URL ^/api -> 공백 변경
